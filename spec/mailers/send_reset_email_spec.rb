7792b3555cdb5a7c4b38e887daf2cb4d5a901e35
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-02 14:22:42 +0100

    Changing post stub to 'recovery_message' for clarity

--- spec/mailers/send_reset_email_spec.rb
+++ spec/mailers/send_reset_email_spec.rb
@@ -4,11 +4,11 @@ describe SendResetEmail do
 
   let(:user)         { double :user, password_token: '4nknkj34nkj23n4j32', email:
                  "user@example.com" }
-  let(:email_client) { double :email_client, post: :sausage }
+  let(:email_client) { double :email_client, post: :recovery_message }
   subject { SendResetEmail }
 
   it 'passes a recovery message to an email client' do
     SendResetEmail::CLIENT = email_client
-    expect(subject.call(user)).to eq :sausage
+    expect(subject.call(user)).to eq :recovery_message
   end
 end
