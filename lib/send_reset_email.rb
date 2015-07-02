807c6d140174a462ec1259d1a5725820757b5b00
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-02 12:04:44 +0100

    Sending the password reset email

--- lib/send_reset_email.rb
+++ lib/send_reset_email.rb
@@ -1,7 +1,20 @@
 class SendResetEmail
 
-  def self.call user
+  def initialize user:, client:
+    @user = user
+    @email_client = client
+  end
 
+  def call
+    @email_client.send_message(email_contents)
+  end
+
+  private
+
+  def email_contents
+    {to:      @user.email,
+    message: "You have requested a password reset. Follow this link to continue:
+      http://www.bookmarkmanager.com/password_reset/#{@user.password_token}"}
   end
 
 end
